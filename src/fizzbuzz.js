1c353bb3949fc4c7941a98f820849fbb5d3c9e31
Angus Townsley <angustownsley@gmail.com>
2024-04-11 12:44:57 +0100

    Add one liner fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,19 +1,17 @@
 /* eslint-disable prettier/prettier */
 // TODO: Write your function in this file
 function fizzBuzz(lower, upper) {
-  const answer = []
+  const arr = []
     for (let i = lower; i <= upper; i++) {
-    if (Number.isInteger(i / 3) & Number.isInteger(i / 5)) {
-      answer.push('FizzBuzz')
-    } else if (Number.isInteger(i / 3)) {
-      answer.push('Fizz')
-    } else if (Number.isInteger(i / 5)) {
-      answer.push('Buzz')
-    } else {
-      answer.push(i)
+      arr.push(i)
     }
+  
+    const answer = arr.map((item) => [['fizz'][item%3],['buzz'][item%5]].join('') || item)
+    
+    return answer
   }
-  return answer
-}
+
+
+
 // TODO: Change undefined below to the name of your function
 module.exports = fizzBuzz
