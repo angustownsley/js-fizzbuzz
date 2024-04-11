f0b6e07fdd609488a24b55341518ce4e9b982930
Angus Townsley <angustownsley@gmail.com>
2024-04-11 13:30:26 +0100

    Add true one liner

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,15 +1,9 @@
 /* eslint-disable prettier/prettier */
 // TODO: Write your function in this file
 function fizzBuzz(lower, upper) {
-  const arr = []
-    for (let i = lower; i <= upper; i++) {
-      arr.push(i)
-    }
   
-    const answer = arr.map((item) => [['fizz'][item%3],['buzz'][item%5]].join('') || item)
-    
-    return answer
-  }
+  return Array.from({length:upper-lower+1},(_,i)=>[['Fizz'][(i+lower)%3],['Buzz'][(i+lower)%5]].join('') || i+lower);
+}
 
 
 
